0a1e0dd3e79095fd9ca0f7a0050673b43f9f2ed5
Ch.Sai Sri Snigdha <snigdhachirumamilla@gmail.com>
2026-04-06 22:32:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -421,7 +421,8 @@ def test():
     return "API WORKING"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
 # from flask import Flask, request, jsonify, session
 # from flask_cors import CORS
